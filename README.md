35dbc6dbcea4a9ee1a2e6477d3350eb88437be63
26234020@qq.com <26234020@qq.com>
2019-07-24 00:30:03 +0800

    add example1-3 and README.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # canvas-upupup
 canvas学习
+
+## 基本用法
+example1-2
+## 绘制图形
+example 3
---
+++ example1/index.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById('tutorial');
+if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+}
